d70bece078aae1d7d76a0bd3fd830c0bea67d280
m1nh101 <minhdd148@gmail.com>
2023-03-05 14:18:15 +0700

    add get session info from token

--- src/WebStore.Application/Services/Users/UserAuthenticationResponse.cs
+++ src/WebStore.Application/Services/Users/UserAuthenticationResponse.cs
@@ -6,4 +6,19 @@ public class UserAuthenticationResponse
   public string Token { get; set; } = string.Empty;
   public string Id { get; set; } = string.Empty;
   public string UserName { get; set; } = string.Empty;
+  public string Avatar { get; set; } = string.Empty;
+}
+
+public class UserData
+{
+  public UserData(string userName, string fullName, string avatar)
+  {
+    UserName = userName;
+    FullName = fullName;
+    Avatar = avatar;
+  }
+
+  public string UserName { get; private set; } = string.Empty;
+  public string FullName { get; private set; } = string.Empty;
+  public string Avatar { get; private set; } = string.Empty;
 }
